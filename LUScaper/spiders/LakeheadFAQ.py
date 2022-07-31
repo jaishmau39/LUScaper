6da5965aea0360193646e0c86fb2d05a01e811e9
Jaishma Usha <jaishmarajan8@gmail.com>
2022-07-31 00:03:44 -0400

    LU Faq update

--- LUScaper/spiders/LakeheadFAQ.py
+++ LUScaper/spiders/LakeheadFAQ.py
@@ -6,21 +6,22 @@ from LUScaper.spiders.items import FAQ
 
 
 class LakeheadSpider(scrapy.Spider):
-    name = 'lakehead'
+    name = 'lakeheadu'
     allowed_domain = ['www.lakeheadu.ca']
-    start_urls = ['https://www.lakeheadu.ca/faculty-and-staff/departments/services/helpdesk/articles/faq']
+    start_urls = ['https://www.lakeheadu.ca/programs/departments/computer-science/future-students/faq']
     custom_settings =  {
-        'FEED_URI' :    'tmp/LUfuturestudents_faq.csv'
+        'FEED_URI' :    'tmp/lakeheadFAQ.csv'
     }
 
+
+
     def parse(self, response):
-        for item in response.xpath("//div[@class='field-items even']//h4"):
+        for item in response.xpath("//div[@class='field-item even']//p"):
             yield {
-                'question' : item.xpath(".//h4/text()").extract(),
-                'answer' : item.xpath(".//p/text()").extract(),
+                'question' : item.xpath(".//strong/text()").extract(),
+                'answer' : item.xpath(".//text()").extract(),
                 'UniversityID' : 'ulakehead'
             }
-
 
 
 
